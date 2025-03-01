bccc7f5d6ba96799a8c17e059bc503046e1aba5c
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-01 15:25:33 -0400

    feat: create user repository

---
+++ backend/mysell/src/main/java/com/project/mysell/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.project.mysell.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+import com.project.mysell.model.UserModel;
+
+public interface UserRepository extends ReactiveCrudRepository<UserModel, UUID>{
+
+}
