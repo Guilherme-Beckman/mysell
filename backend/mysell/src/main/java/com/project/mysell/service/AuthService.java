1bce51e379f1bcc08f03cdc3a67cd734b93e7b2a
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-02 17:39:07 -0400

    refactor: refactor AuthService, delete duplicate code

--- backend/mysell/src/main/java/com/project/mysell/service/AuthService.java
+++ backend/mysell/src/main/java/com/project/mysell/service/AuthService.java
@@ -1,8 +1,6 @@
 package com.project.mysell.service;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -35,22 +33,22 @@ public class AuthService {
     @Autowired 
     private JwtTokenProvider tokenProvider;
 
+    // Método para gerar o token e responder com o ResponseDTO
+    private Mono<ResponseDTO> generateTokenAndRespond(String email, String password) {
+        return this.reactiveAuthenticationManager
+            .authenticate(new UsernamePasswordAuthenticationToken(email, password))
+            .flatMap(authentication -> {
+                String token = this.tokenProvider.createToken(authentication);
+                return Mono.just(new ResponseDTO(email, token));
+            });
+    }
+
     public Mono<ResponseDTO> login(LoginDTO loginDTO) {
         return this.userRepository.findByEmail(loginDTO.email())
-        	/*Usar flatmap para poder aplicar operações*/
             .flatMap(userModel -> {
-            	//pegar o userModel que ta dendro do mono sem interromper fluxo 
-            	//comparar senhas com password encoder
+                // Comparar senhas com o password encoder
                 if (passwordEncoder.matches(loginDTO.password(), userModel.getPassword())) {
-                    return this.reactiveAuthenticationManager
-                    	//vai criar um objeto de autenticação para poder passar como parametro no createToken	
-                        .authenticate(new UsernamePasswordAuthenticationToken(loginDTO.email(), loginDTO.password()))
-                        //novamente aplicar flatmap para aplicar operações no objeto "dentro" do mono
-                        .flatMap(authentication -> {
-                            String token = this.tokenProvider.createToken(authentication);
-                            //retorna o token por meio de um responseDto
-                            return Mono.just(new ResponseDTO(userModel.getEmail(), token));
-                        });
+                    return this.generateTokenAndRespond(loginDTO.email(), loginDTO.password());
                 } else {
                     return Mono.error(new InvalidCredentialsException());
                 }
@@ -59,31 +57,15 @@ public class AuthService {
     }
 
     public Mono<ResponseDTO> register(UserDTO userDTO) {
-    	//chama o repo pra poder verificar se existe um usuario
+        // Verifica se o usuário já existe
         return this.userRepository.findByEmail(userDTO.email())
-            //se existir, ele joga um erro, mas garante que o erro segue um padrãp
-        		.<ResponseDTO>flatMap(existingUser -> {
-                return Mono.error(new UserAlreadyExistsException());
-            })
-        		//caso esteja vazio, usa um defer, que só usado caso o user esteja vazio
+            .<ResponseDTO>flatMap(existingUser -> Mono.error(new UserAlreadyExistsException()))
             .switchIfEmpty(Mono.defer(() -> {
-            	//encode de senha
+                // Criação do novo usuário
                 UserDTO newUser = new UserDTO(userDTO.email(), passwordEncoder.encode(userDTO.password()));
                 UserModel newUserModel = new UserModel(newUser);
-                //salva o novo usuario
                 return this.userRepository.save(newUserModel)
-                		//inicia um flatmap pra lidar com o usuario novo e comecar a gerar o UsernamePasswordAuthenticationToken
-                    .flatMap(savedUser -> 
-                        this.reactiveAuthenticationManager
-                            .authenticate(new UsernamePasswordAuthenticationToken(
-                                userDTO.email(), 
-                                userDTO.password()
-                            ))
-                            .flatMap(authentication -> {
-                                String token = this.tokenProvider.createToken(authentication);
-                                return Mono.just(new ResponseDTO(newUserModel.getEmail(), token));
-                                })
-                    );
+                    .flatMap(savedUser -> this.generateTokenAndRespond(userDTO.email(), userDTO.password()));
             }));
     }
 }
