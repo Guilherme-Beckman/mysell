75a86475556acbdd555e9731cd1bbcb16485850a
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-11 14:22:33 -0400

    feat: add endpoint to retrieve daily report with authorization header

--- backend/mysell/src/main/java/com/project/mysell/controller/ReportController.java
+++ backend/mysell/src/main/java/com/project/mysell/controller/ReportController.java
@@ -1,11 +1,21 @@
 package com.project.mysell.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.mysell.service.ReportService;
+
+import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/report")
 public class ReportController {
 	private ReportService reportService;
+	@GetMapping("/daily")
+	private ResponseEntity<Mono<DailyReportDTO>> getDailyReport (@RequestHeader("Authorization") String token){
+		Mono<DailyReportDTO> dailyReport = this.reportService.getDailyReport(token);
+		return ResponseEntity.ok().body(dailyReport);
+	}
 }
