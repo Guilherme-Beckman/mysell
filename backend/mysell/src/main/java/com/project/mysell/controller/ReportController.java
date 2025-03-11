2221f8e6d5ef1a6979aad89a2102a4cffe5580b3
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-11 14:13:50 -0400

    feat: enhance ReportController with RESTful annotations and inject ReportService

--- backend/mysell/src/main/java/com/project/mysell/controller/ReportController.java
+++ backend/mysell/src/main/java/com/project/mysell/controller/ReportController.java
@@ -1,5 +1,11 @@
 package com.project.mysell.controller;
 
-public class ReportController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.project.mysell.service.ReportService;
+@RestController
+@RequestMapping("/report")
+public class ReportController {
+	private ReportService reportService;
 }
