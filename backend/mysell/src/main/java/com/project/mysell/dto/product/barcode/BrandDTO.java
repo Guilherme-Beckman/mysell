06dff5f5c6d607e4c535dacf5c8e579cc78791ec
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-20 16:32:54 -0400

    feat: add JsonIgnoreProperties annotation to BrandDTO for improved JSON handling

--- backend/mysell/src/main/java/com/project/mysell/dto/product/barcode/BrandDTO.java
+++ backend/mysell/src/main/java/com/project/mysell/dto/product/barcode/BrandDTO.java
@@ -1,5 +1,8 @@
 package com.project.mysell.dto.product.barcode;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
  public record BrandDTO(
     String name,
     String picture
