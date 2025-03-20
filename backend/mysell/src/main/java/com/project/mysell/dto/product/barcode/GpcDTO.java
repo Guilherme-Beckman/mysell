4ad0a498a492e3d68947858247a002079344cb85
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-20 16:32:58 -0400

    feat: add JsonIgnoreProperties annotation to GpcDTO for improved JSON handling

--- backend/mysell/src/main/java/com/project/mysell/dto/product/barcode/GpcDTO.java
+++ backend/mysell/src/main/java/com/project/mysell/dto/product/barcode/GpcDTO.java
@@ -1,4 +1,8 @@
 package com.project.mysell.dto.product.barcode;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
  public record GpcDTO(
     String code,
     String description
