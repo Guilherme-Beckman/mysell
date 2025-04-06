b6af122264febd5be8a30c7921e4f202a82d39ac
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-04-06 15:03:00 -0400

    feat: implement login method in AuthServiceService

--- frontend/mysell/src/app/services/auth-service.service.ts
+++ frontend/mysell/src/app/services/auth-service.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
+  private apiUrl = "http://localhost:8080/";
+  constructor(private httpClient: HttpClient)  {}
 
-  constructor() { }
+  login(email: string, password: string): Observable<any> {
+   const headers = new HttpHeaders().set('Content-Type', 'application/json');
+   const body = JSON.stringify({ email, password });
+    return this.httpClient.post<any>(`${this.apiUrl}login`, body, { headers });
+  }
 }
