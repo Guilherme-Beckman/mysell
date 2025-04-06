82d81b1075c1ee19704f4bb61fe0c025cab888a9
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-04-06 16:50:01 -0400

    feat: add MessagePerRequest component with HTML, SCSS, and unit tests; implement AuthService with login method

---
+++ frontend/mysell/src/app/components/message-per-request/message-per-request.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message-per-request',
+  templateUrl: './message-per-request.component.html',
+  styleUrls: ['./message-per-request.component.scss'],
+  imports: [CommonModule]
+})
+export class MessagePerRequestComponent  implements OnInit {
+  @Input() successMessage: string= '' ;    
+  @Input() errorMessage: string= '' ; 
+  constructor() { }
+
+  ngOnInit() {}
+
+  
+
+}
