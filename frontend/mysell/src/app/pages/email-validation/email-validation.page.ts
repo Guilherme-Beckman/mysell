ee0efd50a4bcc7eb23e46b38ea2f52718ba467ba
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-04-09 10:13:29 -0400

    feat: implement email validation page with HTML structure and SCSS styling

--- frontend/mysell/src/app/pages/email-validation/email-validation.page.ts
+++ frontend/mysell/src/app/pages/email-validation/email-validation.page.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { CodeSquaresComponent } from 'src/app/components/code-squares/code-squares.component';
 
 @Component({
   selector: 'app-email-validation',
   templateUrl: './email-validation.page.html',
   styleUrls: ['./email-validation.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [CommonModule, CodeSquaresComponent],
 })
 export class EmailValidationPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
