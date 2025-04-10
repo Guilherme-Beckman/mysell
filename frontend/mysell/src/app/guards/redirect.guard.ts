8b6f5bc6c9a290046fef0405fd2ed080d2a6e7f3
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-04-10 15:25:54 -0400

    refactor: clean up comments in redirect guard for clarity

--- frontend/mysell/src/app/guards/redirect.guard.ts
+++ frontend/mysell/src/app/guards/redirect.guard.ts
@@ -9,9 +9,7 @@ export const redirectGuard: CanActivateFn = (
   const emailToValidate = localStorage.getItem('emailToValidate');
 
   if (emailToValidate) {
-    // Retorna UrlTree, e o Angular redireciona para '/email-validation'
     return router.parseUrl('/email-validation');
   }
-  // Caso contrário, retorna UrlTree redirecionando para '/login'
   return router.parseUrl('/login');
 };
