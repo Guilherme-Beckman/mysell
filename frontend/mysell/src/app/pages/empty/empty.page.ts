9fee4a3ebdd982a56a476ec1b9146bc585d88bf3
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-04-11 09:25:05 -0400

    fix: update API URL in production environment configuration

--- frontend/mysell/src/app/pages/empty/empty.page.ts
+++ frontend/mysell/src/app/pages/empty/empty.page.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-empty',
   templateUrl: './empty.page.html',
   styleUrls: ['./empty.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [],
 })
 export class EmptyPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- frontend/mysell/src/environments/environment.prod.ts
+++ frontend/mysell/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl:
-    'https://c4e9-2804-6194-1f74-d900-9ac0-d550-50b7-8aa0.ngrok-free.app/',
+  apiUrl: 'https://7280-2804-6194-1f74-d900-19b-eae5-19d4-9973.ngrok-free.app/',
 };
