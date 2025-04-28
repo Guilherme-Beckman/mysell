d4674464ec7ed5208fbfc65334dd559ba41eac5f
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-04-28 16:18:24 -0400

    feat: add SellsProfitInfoComponent with template, styles, and tests

---
+++ frontend/mysell/src/app/componets/sells-profit-info/sells-profit-info.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sells-profit-info',
+  templateUrl: './sells-profit-info.component.html',
+  styleUrls: ['./sells-profit-info.component.scss'],
+})
+export class SellsProfitInfoComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
