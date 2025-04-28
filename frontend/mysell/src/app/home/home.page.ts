0b155e3ebccc3af7cb89a49b6df63abaf905e0c1
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-04-28 16:21:12 -0400

    feat: integrate SellsProfitInfoComponent into HomePage and update template

--- frontend/mysell/src/app/home/home.page.ts
+++ frontend/mysell/src/app/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { HomeNavComponent } from '../components/home-nav/home-nav.component';
+import { SellsProfitInfoComponent } from '../componets/sells-profit-info/sells-profit-info.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, HomeNavComponent],
+  imports: [HomeNavComponent, SellsProfitInfoComponent],
 })
 export class HomePage {
   constructor() {}
