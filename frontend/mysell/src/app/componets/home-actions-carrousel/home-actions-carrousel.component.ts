4659744feafb988e26414fb29f45ffc630086489
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-04-28 16:56:00 -0400

    feat: implement HomeActionsCarrouselComponent with action buttons and routing functionality

--- frontend/mysell/src/app/componets/home-actions-carrousel/home-actions-carrousel.component.ts
+++ frontend/mysell/src/app/componets/home-actions-carrousel/home-actions-carrousel.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonIcon } from '@ionic/angular/standalone';
 
+export interface ActionButton {
+  icon: string; // Exemplo: 'arrow-up-circle-outline'
+  label: string; // Exemplo: 'Vender'
+  route: string; // Exemplo: '/vendas'
+  cssClass?: string; // Classe CSS opcional para customização
+}
 @Component({
   selector: 'app-home-actions-carrousel',
   templateUrl: './home-actions-carrousel.component.html',
   styleUrls: ['./home-actions-carrousel.component.scss'],
+  imports: [IonIcon, CommonModule],
 })
-export class HomeActionsCarrouselComponent  implements OnInit {
+export class HomeActionsCarrouselComponent implements OnInit {
+  @Input() actions: ActionButton[] = [];
+  ngOnInit(): void {}
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  onAction(action: ActionButton) {
+    if (action.route) {
+      this.router.navigateByUrl(action.route);
+    }
+  }
 }
