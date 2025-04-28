aaed1349cd56c199e532c96752b7e11f0d2df2e1
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-04-28 14:12:58 -0400

    feat: enhance HomeNavComponent with SVG icons and styling adjustments

--- frontend/mysell/src/app/components/home-nav/home-nav.component.ts
+++ frontend/mysell/src/app/components/home-nav/home-nav.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { IonSearchbar } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-home-nav',
   templateUrl: './home-nav.component.html',
   styleUrls: ['./home-nav.component.scss'],
+  imports: [IonSearchbar],
 })
 export class HomeNavComponent  implements OnInit {
 
