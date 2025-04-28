f0a9fe3d8c51a85cbb241ebf1f160a24f578452e
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-04-28 14:05:59 -0400

    feat: add HomeNavComponent with template and styles for navigation

--- frontend/mysell/src/app/home/home.page.ts
+++ frontend/mysell/src/app/home/home.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { HomeNavComponent } from '../components/home-nav/home-nav.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, HomeNavComponent],
 })
 export class HomePage {
   constructor() {}
