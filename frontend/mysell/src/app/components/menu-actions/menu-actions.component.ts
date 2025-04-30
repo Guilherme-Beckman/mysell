b2f4c7389d11bf8930e66c3c228e3abb814069f3
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-04-30 16:43:31 -0400

    feat: implement menu actions component with dynamic actions and event handling

--- frontend/mysell/src/app/components/menu-actions/menu-actions.component.ts
+++ frontend/mysell/src/app/components/menu-actions/menu-actions.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { IonItem } from '@ionic/angular/standalone';
+import { IonMenu, IonList, IonIcon, IonLabel } from '@ionic/angular/standalone';
+// menu-action.model.ts
+export interface MenuAction {
+  icon: string; // nome do ícone (pode ser do Ionicons ou Remix Icons)
+  label: string; // texto que aparece no menu
+  handler?: () => void; // função opcional a ser executada ao clicar
+}
 
 @Component({
   selector: 'app-menu-actions',
   templateUrl: './menu-actions.component.html',
   styleUrls: ['./menu-actions.component.scss'],
+  imports: [IonList, IonItem, IonIcon, IonLabel, CommonModule],
 })
-export class MenuActionsComponent  implements OnInit {
-
-  constructor() { }
+export class MenuActionsComponent implements OnInit {
+  constructor() {}
 
   ngOnInit() {}
+  @Input() actions: MenuAction[] = [];
+  @Output() actionSelected = new EventEmitter<MenuAction>();
 
+  onClick(action: MenuAction) {
+    if (action.handler) {
+      action.handler();
+    }
+    this.actionSelected.emit(action);
+  }
 }
--- frontend/mysell/src/app/components/menu-bar/menu-bar.component.ts
+++ frontend/mysell/src/app/components/menu-bar/menu-bar.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
-import { IonMenu } from '@ionic/angular/standalone';
+import {
+  IonMenu,
+  IonList,
+  IonItem,
+  IonIcon,
+  IonLabel,
+} from '@ionic/angular/standalone';
+import {
+  MenuAction,
+  MenuActionsComponent,
+} from '../menu-actions/menu-actions.component';
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
   styleUrls: ['./menu-bar.component.scss'],
-  imports: [IonMenu],
+  imports: [IonMenu, MenuActionsComponent],
 })
 export class MenuBarComponent implements OnInit {
   public email: string = '';
+  menuActions: MenuAction[] = [
+    {
+      icon: 'bar-chart-outline',
+      label: 'Análise de vendas',
+      handler: () => console.log('Análise'),
+    },
+    {
+      icon: 'contrast-outline',
+      label: 'Alterar tema',
+      handler: () => console.log('Tema'),
+    },
+    {
+      icon: 'add-circle-outline',
+      label: 'Adicionar Produto',
+      handler: () => console.log('Add produto'),
+    },
+    {
+      icon: 'time-outline',
+      label: 'Histórico de vendas',
+      handler: () => console.log('Histórico'),
+    },
+    {
+      icon: 'document-text-outline',
+      label: 'Relatório',
+      handler: () => console.log('Relatório'),
+    },
+    {
+      icon: 'help-circle-outline',
+      label: 'Ajuda',
+      handler: () => console.log('Ajuda'),
+    },
+    {
+      icon: 'log-out-outline',
+      label: 'Sair',
+      handler: () => console.log('Sair'),
+    },
+  ];
   ngOnInit() {
     this.email = localStorage.getItem('email') || '';
   }
@@ -18,5 +65,9 @@ export class MenuBarComponent implements OnInit {
   async open(route: string) {
     await this.menu.close('main-menu');
     this.router.navigate([route]);
+  }
+  onMenuAction(action: MenuAction) {
+    // aqui você pode, por exemplo, navegar ou chamar serviços
+    console.log('Clicou em', action.label);
   }
 }
