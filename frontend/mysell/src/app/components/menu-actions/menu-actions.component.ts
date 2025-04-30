417801d7368d34650182992832d79fbae76e31e1
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-04-30 15:05:57 -0400

    feat: add MenuActionsComponent with initial template and test setup

---
+++ frontend/mysell/src/app/components/menu-actions/menu-actions.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-actions',
+  templateUrl: './menu-actions.component.html',
+  styleUrls: ['./menu-actions.component.scss'],
+})
+export class MenuActionsComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
