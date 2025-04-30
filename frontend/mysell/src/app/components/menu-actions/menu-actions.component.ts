7fd899f65f5929c822dab28ba1fa9f7c94dee768
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-04-30 17:30:04 -0400

    refactor: reorganize imports and structure in menu actions component; update menu bar layout

--- frontend/mysell/src/app/components/menu-actions/menu-actions.component.ts
+++ frontend/mysell/src/app/components/menu-actions/menu-actions.component.ts
@@ -1,26 +1,26 @@
-import { CommonModule } from '@angular/common';
+// src/app/menu-actions/menu-actions.component.ts
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { IonItem } from '@ionic/angular/standalone';
-import { IonMenu, IonList, IonIcon, IonLabel } from '@ionic/angular/standalone';
-// menu-action.model.ts
+import { CommonModule } from '@angular/common';
+import { IonList, IonItem, IonIcon, IonLabel } from '@ionic/angular/standalone';
+// src/app/menu-actions/menu-action.model.ts
 export interface MenuAction {
-  icon: string; // nome do ícone (pode ser do Ionicons ou Remix Icons)
-  label: string; // texto que aparece no menu
-  handler?: () => void; // função opcional a ser executada ao clicar
+  icon: string; // nome do ícone (Ionicons ou Remix Icons)
+  label: string; // texto exibido
+  handler?: () => void; // função opcional ao clicar
 }
 
 @Component({
   selector: 'app-menu-actions',
   templateUrl: './menu-actions.component.html',
   styleUrls: ['./menu-actions.component.scss'],
-  imports: [IonList, IonItem, IonIcon, IonLabel, CommonModule],
+  standalone: true,
+  imports: [CommonModule, IonList, IonItem, IonIcon, IonLabel],
 })
 export class MenuActionsComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
   @Input() actions: MenuAction[] = [];
   @Output() actionSelected = new EventEmitter<MenuAction>();
+
+  ngOnInit() {}
 
   onClick(action: MenuAction) {
     if (action.handler) {
