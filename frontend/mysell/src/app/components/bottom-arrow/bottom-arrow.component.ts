fa86ec4df4a7bb55fc2cc64629c1105bfa8b9a20
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-05-01 19:32:22 -0400

    feat: add BottomArrowComponent with template, styles, and initial tests

---
+++ frontend/mysell/src/app/components/bottom-arrow/bottom-arrow.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bottom-arrow',
+  templateUrl: './bottom-arrow.component.html',
+  styleUrls: ['./bottom-arrow.component.scss'],
+})
+export class BottomArrowComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
