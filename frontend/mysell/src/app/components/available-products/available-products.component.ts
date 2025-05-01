d5bc9124f66c0c54d9c7082bd2f37d3e4a4ecbe7
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-05-01 12:00:10 -0400

    feat: add AvailableProductsComponent with template, styles, and initial tests

---
+++ frontend/mysell/src/app/components/available-products/available-products.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-available-products',
+  templateUrl: './available-products.component.html',
+  styleUrls: ['./available-products.component.scss'],
+})
+export class AvailableProductsComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
