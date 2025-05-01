47705e79f925a8decc525430d2163348eee6d356
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-05-01 09:36:31 -0400

    feat: remove HomeComponent and add HomeRedirectComponent with initial setup

--- frontend/mysell/src/app/components/home-redirect/home-redirect.component.spec.ts
+++ frontend/mysell/src/app/components/home-redirect/home-redirect.component.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { HomeComponent } from './home.component';
+import { HomeRedirectComponent } from './home-redirect.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('HomeRedirectComponent', () => {
+  let component: HomeRedirectComponent;
+  let fixture: ComponentFixture<HomeRedirectComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ],
+      declarations: [ HomeRedirectComponent ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(HomeRedirectComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
---
+++ frontend/mysell/src/app/components/home-redirect/home-redirect.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-redirect',
+  templateUrl: './home-redirect.component.html',
+  styleUrls: ['./home-redirect.component.scss'],
+})
+export class HomeRedirectComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
