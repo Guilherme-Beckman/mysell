6bd473c2aee064521347e271548f6ed8bf1faf3f
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-05-05 16:38:21 -0400

    feat: add ConfirmPopUpComponent with template, styles, and tests

---
+++ frontend/mysell/src/app/components/confirm-pop-up/confirm-pop-up.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-pop-up',
+  templateUrl: './confirm-pop-up.component.html',
+  styleUrls: ['./confirm-pop-up.component.scss'],
+})
+export class ConfirmPopUpComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
