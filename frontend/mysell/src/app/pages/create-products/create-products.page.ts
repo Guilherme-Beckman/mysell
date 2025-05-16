48b1ec2794630cd9faf7c477f3cf1a2964c6c044
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-05-16 15:59:49 -0400

    feat: add barcode scanning functionality to create products page

--- frontend/mysell/src/app/pages/create-products/create-products.page.ts
+++ frontend/mysell/src/app/pages/create-products/create-products.page.ts
@@ -18,7 +18,8 @@ import { CreateProductFormComponent } from 'src/app/components/create-product-fo
 import { ProductSelectionService } from 'src/app/services/product-selection.service';
 import { Product } from 'src/app/interfaces/product';
 import { ProguessBarComponent } from 'src/app/components/proguess-bar/proguess-bar.component';
-
+import { Barcode, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
+import { AlertController } from '@ionic/angular/standalone';
 @Component({
   selector: 'app-create-products',
   templateUrl: './create-products.page.html',
@@ -53,18 +54,24 @@ export class CreateProductsPage implements OnInit {
     },
     {
       svgPath: '/assets/svg/barcode.svg',
-      action: () => null,
+      action: () => this.scan(),
     },
   ];
 
+  public isSupported = false;
+  public barcodes: Barcode[] = [];
   constructor(
     private navController: NavController,
-    private productSelectionService: ProductSelectionService
+    private productSelectionService: ProductSelectionService,
+    private alertController: AlertController
   ) {}
 
   ngOnInit() {
     this.initializeSelectedProducts();
     this.animateProgress();
+    BarcodeScanner.isSupported().then((result) => {
+      this.isSupported = result.supported;
+    });
   }
 
   public onSearchChange(term: string): void {
@@ -104,5 +111,29 @@ export class CreateProductsPage implements OnInit {
 
   private navigateToEditPage(): void {
     this.navController.navigateRoot('/edit-available-products');
+  }
+
+  async scan(): Promise<void> {
+    const granted = await this.requestPermissions();
+    if (!granted) {
+      this.presentAlert();
+      return;
+    }
+    const { barcodes } = await BarcodeScanner.scan();
+    this.barcodes.push(...barcodes);
+  }
+
+  async requestPermissions(): Promise<boolean> {
+    const { camera } = await BarcodeScanner.requestPermissions();
+    return camera === 'granted' || camera === 'limited';
+  }
+
+  async presentAlert(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Permission denied',
+      message: 'Please grant camera permission to use the barcode scanner.',
+      buttons: ['OK'],
+    });
+    await alert.present();
   }
 }
