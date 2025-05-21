df4d8ebd973c66a41f5abb1fb3dcb81f1b56bfdf
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-05-21 15:12:54 -0400

    feat: add BottomTrashcan component with HTML template, SCSS styling, and unit tests

---
+++ frontend/mysell/src/app/components/bottom-trashcan/bottom-trashcan.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bottom-trashcan',
+  templateUrl: './bottom-trashcan.component.html',
+  styleUrls: ['./bottom-trashcan.component.scss'],
+})
+export class BottomTrashcanComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
