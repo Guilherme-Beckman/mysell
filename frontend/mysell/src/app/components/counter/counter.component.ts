0bd3378e91a3a04281178701e9edd5674d0ef17e
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-05-27 15:39:49 -0400

    feat: implement CounterComponent with increment/decrement functionality; update SellPage layout and integrate components

--- frontend/mysell/src/app/components/counter/counter.component.ts
+++ frontend/mysell/src/app/components/counter/counter.component.ts
@@ -1,14 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-counter',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.scss'],
 })
-export class CounterComponent  implements OnInit {
+export class CounterComponent {
+  /** Valor atual */
+  @Input() count = 0;
+  /** Valor mínimo permitido */
+  @Input() min = 0;
+  /** Valor máximo permitido */
+  @Input() max = Infinity;
+  /** Desabilita todo o controle */
+  @Input() disabled = false;
+  /** Emite sempre que o count mudar */
+  @Output() countChange = new EventEmitter<number>();
 
-  constructor() { }
+  decrement(): void {
+    if (this.disabled) {
+      return;
+    }
+    const next = this.count - 1;
+    if (next >= this.min) {
+      this.count = next;
+      this.countChange.emit(this.count);
+    }
+  }
 
-  ngOnInit() {}
-
+  increment(): void {
+    if (this.disabled) {
+      return;
+    }
+    const next = this.count + 1;
+    if (next <= this.max) {
+      this.count = next;
+      this.countChange.emit(this.count);
+    }
+  }
 }
--- frontend/mysell/src/app/pages/sell/sell.page.ts
+++ frontend/mysell/src/app/pages/sell/sell.page.ts
@@ -1,20 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { CounterComponent } from 'src/app/components/counter/counter.component';
+import { LoadingSppinerComponent } from 'src/app/components/loading-sppiner/loading-sppiner.component';
+import { MessagePerRequestComponent } from 'src/app/components/message-per-request/message-per-request.component';
+import { SearchBarComponent } from 'src/app/components/search-bar/search-bar.component';
+import { ConfirmButtonComponent } from 'src/app/components/confirm-button/confirm-button.component';
+import { ConfirmPopUpComponent } from 'src/app/components/confirm-pop-up/confirm-pop-up.component';
+import { HomeRedirectComponent } from 'src/app/components/home-redirect/home-redirect.component';
+import { ArrowComponent } from 'src/app/components/arrow/arrow.component';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-sell',
   templateUrl: './sell.page.html',
   styleUrls: ['./sell.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    CounterComponent,
+    LoadingSppinerComponent,
+    MessagePerRequestComponent,
+    SearchBarComponent,
+    ConfirmPopUpComponent,
+    ArrowComponent,
+    HomeRedirectComponent,
+  ],
 })
 export class SellPage implements OnInit {
+  public isLoading = false;
+  public searchTerm: string = '';
+  public successMessage$ = this.messageService.successMessage$;
+  public errorMessage$ = this.messageService.errorMessage$;
+  constructor(private messageService: MessageService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  public onSearchChange(term: string): void {
+    this.searchTerm = term;
   }
-
 }
