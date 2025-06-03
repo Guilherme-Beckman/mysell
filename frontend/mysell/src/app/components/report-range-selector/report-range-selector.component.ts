8a3449afd03c187fb2e0bd97da7cf11869de60c4
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-06-03 14:32:23 -0400

    feat: add ReportRangeSelectorComponent with template, styles, and unit tests

---
+++ frontend/mysell/src/app/components/report-range-selector/report-range-selector.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-report-range-selector',
+  templateUrl: './report-range-selector.component.html',
+  styleUrls: ['./report-range-selector.component.scss'],
+})
+export class ReportRangeSelectorComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
