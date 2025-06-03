bab2bb49e8f6609192e50aeb268a81411b1e8227
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-06-03 14:27:04 -0400

    feat: update routing for AnalyticsPage and enhance its structure; add missing components and styles

--- frontend/mysell/src/app/pages/analytics/analytics.page.ts
+++ frontend/mysell/src/app/pages/analytics/analytics.page.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { ArrowComponent } from 'src/app/components/arrow/arrow.component';
+import { HomeRedirectComponent } from 'src/app/components/home-redirect/home-redirect.component';
 
 @Component({
   selector: 'app-analytics',
   templateUrl: './analytics.page.html',
   styleUrls: ['./analytics.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [ArrowComponent, HomeRedirectComponent, CommonModule, FormsModule],
 })
 export class AnalyticsPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
