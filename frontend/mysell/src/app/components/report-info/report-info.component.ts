0708790901e9d6ac255d4cdba46708830ecf97e7
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-06-03 17:46:04 -0400

    feat: update routing to load AnalyticsPage and enhance ReportInfoComponent layout and styles

--- frontend/mysell/src/app/components/report-info/report-info.component.ts
+++ frontend/mysell/src/app/components/report-info/report-info.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, input, OnInit } from '@angular/core';
-
+export interface ProductNameSales {
+  name: string;
+  sales: number;
+}
 @Component({
   selector: 'app-report-info',
   templateUrl: './report-info.component.html',
   styleUrls: ['./report-info.component.scss'],
+  imports: [CommonModule],
 })
 export class ReportInfoComponent implements OnInit {
-  @Input() report: any;
+  @Input() profit: number = 0;
+  @Input() sales: number = 0;
+  @Input() productRanking: ProductNameSales[] = [];
+
   constructor() {}
 
   ngOnInit() {}
