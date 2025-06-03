545053b6c8e9d19caa7085e0dbd7999d90a5e2e5
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-06-03 18:10:10 -0400

    feat: implement ProductRankingComponent with template, styles, and sample data

--- frontend/mysell/src/app/components/product-ranking/product-ranking.component.ts
+++ frontend/mysell/src/app/components/product-ranking/product-ranking.component.ts
@@ -1,14 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ProductNameSales } from '../report-info/report-info.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-ranking',
   templateUrl: './product-ranking.component.html',
   styleUrls: ['./product-ranking.component.scss'],
+  imports: [CommonModule],
 })
-export class ProductRankingComponent  implements OnInit {
+export class ProductRankingComponent implements OnInit {
+  @Input() productRanking: ProductNameSales[] = [];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  ngOnInit() {
+    this.productRanking = [
+      { name: 'Arroz Tio João', sales: 15 },
+      { name: 'Feijão Camil', sales: 12 },
+      { name: 'Macarrão Renata', sales: 9 },
+      { name: 'Açúcar União', sales: 7 },
+      { name: 'Café Pilão', sales: 5 },
+    ];
+  }
 }
