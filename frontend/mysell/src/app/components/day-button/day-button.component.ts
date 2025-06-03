ca8b63b0f9335321a8359fd6f870a4b4d73336b5
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-06-03 14:40:07 -0400

    feat: add DayButtonComponent with template, styles, and unit tests

---
+++ frontend/mysell/src/app/components/day-button/day-button.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-day-button',
+  templateUrl: './day-button.component.html',
+  styleUrls: ['./day-button.component.scss'],
+})
+export class DayButtonComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
