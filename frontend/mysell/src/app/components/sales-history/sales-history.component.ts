1a74ad55a70a26ec2600da04581a367c67b3587e
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-06-07 17:35:16 -0400

    feat: implement confirmation pop-up for sale deletion in SalesHistory component

--- frontend/mysell/src/app/components/sales-history/sales-history.component.ts
+++ frontend/mysell/src/app/components/sales-history/sales-history.component.ts
@@ -4,19 +4,35 @@ import { SellService } from 'src/app/services/sell.service';
 import { getCategoryIconPath } from 'src/app/datas/categories';
 import { IonSkeletonText } from '@ionic/angular/standalone';
 import { ConfirmPopUpComponent } from '../confirm-pop-up/confirm-pop-up.component';
+import { LoadingSppinerComponent } from '../loading-sppiner/loading-sppiner.component';
+import { MessageService } from 'src/app/services/message.service';
+import { MessagePerRequestComponent } from '../message-per-request/message-per-request.component';
 @Component({
   selector: 'app-sales-history',
   standalone: true,
   templateUrl: './sales-history.component.html',
   styleUrls: ['./sales-history.component.scss'],
-  imports: [IonSkeletonText, CommonModule, ConfirmPopUpComponent],
+  imports: [
+    IonSkeletonText,
+    CommonModule,
+    ConfirmPopUpComponent,
+    LoadingSppinerComponent,
+    MessagePerRequestComponent,
+  ],
 })
 export class SalesHistoryComponent implements OnInit {
   sales: any[] = [];
   filter: string = '24h';
   isLoading = false;
+  isLoadingActions = false;
   isConfirmPopupOpen = false;
-  constructor(private sellService: SellService) {}
+  saleIdToDelete: string | null = null;
+  public successMessage$ = this.messageService.successMessage$;
+  public errorMessage$ = this.messageService.errorMessage$;
+  constructor(
+    private sellService: SellService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.isLoading = true;
@@ -65,5 +81,65 @@ export class SalesHistoryComponent implements OnInit {
     return getCategoryIconPath(categoryName);
     {
     }
+  }
+  openConfirmPopup(saleId: string) {
+    console.log('[Popup] Abrindo popup de confirmação para saleId:', saleId);
+    this.saleIdToDelete = saleId;
+    this.isConfirmPopupOpen = true;
+  }
+
+  closeConfirmPopup() {
+    console.log('[Popup] Fechando popup de confirmação.');
+    this.isConfirmPopupOpen = false;
+  }
+
+  confirmSaleDeletion() {
+    this.isLoadingActions = true;
+
+    if (!this.saleIdToDelete) {
+      console.warn('[Delete] Nenhuma saleId definida para deletar.');
+      this.isLoadingActions = false;
+      return;
+    }
+
+    console.log(
+      '[Delete] Iniciando deleção da venda com saleId:',
+      this.saleIdToDelete
+    );
+
+    this.sellService.deleteSell(Number(this.saleIdToDelete)).subscribe({
+      next: () => {
+        console.log(
+          '[Delete] Venda deletada com sucesso:',
+          this.saleIdToDelete
+        );
+        console.log('[Delete] Lista de vendas antes da exclusão:', this.sales);
+
+        this.sales = this.sales.filter(
+          (sale) => sale.sellId !== this.saleIdToDelete
+        );
+
+        console.log('[Delete] Lista de vendas após a exclusão:', this.sales);
+
+        this.messageService.setSuccessMessage(
+          'Venda deletada com sucesso.',
+          ''
+        );
+        this.isLoadingActions = false;
+      },
+      error: (err) => {
+        console.error('[Delete] Erro ao deletar venda:', err);
+        this.messageService.setErrorMessage(
+          'Erro ao deletar a venda. Tente novamente.',
+          ''
+        );
+        this.isLoadingActions = false;
+      },
+      complete: () => {
+        console.log('[Delete] Finalizando processo de deleção.');
+        this.isConfirmPopupOpen = false;
+        this.saleIdToDelete = null;
+      },
+    });
   }
 }
