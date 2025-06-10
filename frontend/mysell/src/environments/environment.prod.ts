b44a9881078963039824afeff419b75e0b991fc7
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-06-10 14:48:09 -0400

    fix: update production API URL in environment configuration

--- frontend/mysell/src/environments/environment.prod.ts
+++ frontend/mysell/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl:
-    'https://9186-2804-6194-1f74-d900-e1f4-76b8-7e0f-61eb.ngrok-free.app/',
+  apiUrl: 'https://f97a-2804-6194-1f74-d900-8215-70f-2545-668.ngrok-free.app/',
 };
