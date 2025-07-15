3523a8425d5e06517f298ded688df9882c41069c
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-07-15 15:47:07 -0400

    feat: add gitignore

--- frontend/mysell/src/environments/environment.prod.ts
+++ frontend/mysell/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://6634-2804-6194-1f74-d900-783-7b2-4bef-1ea3.ngrok-free.app/',
+  apiUrl: 'https://852bce5b5159.ngrok-free.app/',
 };
